861e57743313c28e2b35ab567ace80e4cc0649b2
David MacIntosh <macintoshdg4@gmail.com>
2021-03-04 10:30:31 +0200

    Made the small list dynamic

--- components/ServicesNav.js
+++ components/ServicesNav.js
@@ -9,6 +9,7 @@ import ImageIcon from '@material-ui/icons/Image';
 import WorkIcon from '@material-ui/icons/Work';
 import BeachAccessIcon from '@material-ui/icons/BeachAccess';
 import { Link } from '@material-ui/core';
+import { useRouter } from 'next/router'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,41 +22,73 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function FolderList() {
-  const classes = useStyles();
+function FolderList() {
+    const classes = useStyles();
+    const router = useRouter();
 
-  return (
-    <List className={classes.root}>
-        <Link href="/services-consolidated-load" className={classes.link}>
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <ImageIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Consolidation Loads" />
-      </ListItem>
-      </Link>
-      <Link href="/services-clearing" className={classes.link}>
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <WorkIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Clearing" />
-      </ListItem>
-      </Link>
-      <Link href="/services-procurement" className={classes.link}>
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <BeachAccessIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Procurement / Facilitation Service" />
-      </ListItem>
-        </Link>
-    </List>
-  );
+    const consolidated = router.pathname === '/services-consolidated-load';
+    const clearing = router.pathname === '/services-clearing';
+    const procurement = router.pathname === '/services-procurement';
+    const fullLoad = router.pathname === '/services-full-load';
+
+    console.log(router.pathname)
+    
+
+    return (
+        <List className={classes.root}>
+            {!consolidated && (
+                <Link href="/services-consolidated-load" className={classes.link}>
+                    <ListItem>
+                        <ListItemAvatar>
+                            <Avatar>
+                                <ImageIcon />
+                            </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText primary="Consolidation Loads" />
+                        </ListItem>
+                </Link>
+            )}
+
+            {!clearing && (
+                <Link href="/services-clearing" className={classes.link}>
+                    <ListItem>
+                        <ListItemAvatar>
+                        <Avatar>
+                            <WorkIcon />
+                        </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText primary="Clearing" />
+                    </ListItem>
+                </Link>
+            )}
+
+            {!procurement && (
+                <Link href="/services-procurement" className={classes.link}>
+                    <ListItem>
+                        <ListItemAvatar>
+                        <Avatar>
+                            <BeachAccessIcon />
+                        </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText primary="Procurement / Facilitation Service" />
+                    </ListItem>
+                    </Link>
+            )}
+
+            {!fullLoad && (
+                <Link href="/services-full-load" className={classes.link}>
+                    <ListItem>
+                        <ListItemAvatar>
+                        <Avatar>
+                            <BeachAccessIcon />
+                        </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText primary="Full Load" />
+                    </ListItem>
+                </Link>
+            )}
+        </List>
+    );
 }
+
+export default (FolderList);
