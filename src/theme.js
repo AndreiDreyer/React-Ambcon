ee87f683788033402ec8b1e3c6693aba9a4c9f49
Andrei Dreyer <andrei.dreyer1997@gmail.com>
2021-03-08 18:30:19 +0200

    Finished top navbar, updated theme breakpoints, bottom footer almost finished

--- src/theme.js
+++ src/theme.js
@@ -1,10 +1,10 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme } from "@material-ui/core/styles";
 
 export const customTheme = createMuiTheme({
   breakpoints: {
     values: {
       xs: 0,
-      sm: 769,
+      sm: 768,
       md: 960,
       lg: 1280,
       xl: 1920,
