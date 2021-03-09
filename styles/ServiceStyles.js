7cfafdaccab68fe5ab2fb905fd857524e1359e9c
David MacIntosh <macintoshdg4@gmail.com>
2021-03-09 16:04:50 +0200

    Moved styles to separate file, this may make changing the banner images difficult

---
+++ styles/ServiceStyles.js
@@ -0,0 +1,105 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { customTheme as theme } from '../src/theme';
+
+const useStyles = makeStyles({
+    mainContainer: {
+        width: '100%',
+    },
+    bannerDiv: {
+        flexGrow: 1,
+        width: '100%',
+        [theme.breakpoints.up('1024')]: {
+        height: 215,
+        },
+        [theme.breakpoints.up('lg')]: {
+        height: 215,
+        marginBottom: 86,
+        },
+        [theme.breakpoints.up('xl')]: {
+        height: 215,
+        marginBottom: 86,
+        },
+    },
+    card: {
+        flexGrow: 1,
+        width: '100%',
+        marginTop: 75,
+    },
+    media: {
+        height: 400,
+    },
+    body: {
+        flexGrow: 1,
+
+        [theme.breakpoints.down('769')]: {
+            width: '100%',
+        },
+        [theme.breakpoints.up('1024')]: {
+            marginLeft: '22%',
+            marginRight: '18%',
+            
+        },
+        
+    },
+    headers: {
+        alignContent: 'center',
+        textAlign: 'left',
+        color: 'white',
+        backgroundColor: '#5eac3f',
+        paddingLeft: 20,
+        paddingBottom: 20,
+
+        [theme.breakpoints.down('769')]: {
+            width: '100%',
+        },
+        [theme.breakpoints.up('1024')]: {
+            marginLeft: '22%',
+            marginRight: '18%',
+        },
+    },
+    textHeaders: {
+        alignContent: 'left',
+        textAlign: 'left',
+        color: '#5eac3f',
+
+        [theme.breakpoints.down('769')]: {
+            width: '100%',
+        },
+        
+    },
+    text: {
+
+    },
+    content: {
+        [theme.breakpoints.down('769')]: {
+            width: '100%',
+        },
+        [theme.breakpoints.up('1024')]: {
+            float: 'left',
+            maxWidth: '60%',
+        },
+    },
+    listBox: {
+        [theme.breakpoints.down('769')]: {
+            alignSelf: 'center',
+        },
+        [theme.breakpoints.up('1024')]: {
+            alignSelf: 'right',
+            float: 'right',
+            maxWidth: '40%',
+        },
+    },
+    bodyBlock: {
+        [theme.breakpoints.down('769')]: {
+            marginTop: '2%',
+        },
+        [theme.breakpoints.up('1024')]: {
+            marginTop: '20%',
+        },
+        [theme.breakpoints.up('1280')]: {
+            marginTop: '11%',
+        },
+    },
+});
+
+export { useStyles };
