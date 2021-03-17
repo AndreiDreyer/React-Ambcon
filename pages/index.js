afbf819ee205f99e5def74b9ef8a48aaf605c53c
Andrei Dreyer <19809301@sun.ac.za>
2021-03-17 09:49:56 +0200

    Home Page (#7)
    * Added banner to home page, updated services with badges, fixed overflow problem on nav
    
    * remove a console.log

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Head from "next/head";
+import Link from "next/link";
+
 import Footer from "../components/Footer";
 import Navigation from "../components/Navigation";
+import ContactForm from "../components/ContactForm";
 
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
@@ -30,14 +33,44 @@ const useStyles = makeStyles((theme) => ({
   servicesContainer: {
     height: "100%",
   },
-  serviceImage: {},
+  serviceImage: {
+    maxHeight: 175,
+  },
   serviceTitle: {
     maxHeight: "75px",
   },
   introText: {
     lineHeight: "1.7rem",
   },
+  bannerImage: {
+    width: "100%",
+    maxWidth: "100%",
+    margin: 0,
+  },
 }));
+
+const services = [
+  {
+    serviceName: "Consolodation",
+    imgUrl: "Badges/consolidated-load.png",
+    serviceUrl: "/services/consolidated-load",
+  },
+  {
+    serviceName: "Full Load",
+    imgUrl: "Badges/full-load.png",
+    serviceUrl: "/services/full-load",
+  },
+  {
+    serviceName: "Clearing",
+    imgUrl: "Badges/clearing.png",
+    serviceUrl: "/services/clearing",
+  },
+  {
+    serviceName: "Procurement",
+    imgUrl: "Badges/procurement.png",
+    serviceUrl: "/services/procurement",
+  },
+];
 
 export default function Home() {
   const classes = useStyles();
@@ -51,35 +84,23 @@ export default function Home() {
 
   const numCols = () => {
     if (ipad && ipadUp) {
-      return 3;
+      return 2;
     } else if (sm) {
       return 1;
     } else if (md) {
-      return 3;
+      return 4;
     } else {
-      return 3;
+      return 4;
     }
   };
-
-  const services = [
-    {
-      serviceName: "Consolodation",
-      imgUrl: "logo.png",
-    },
-    {
-      serviceName: "Full Load",
-      imgUrl: "logo.png",
-    },
-    {
-      serviceName: "Clearing",
-      imgUrl: "logo.png",
-    },
-  ];
 
   return (
     <div className={classes.root}>
       <Navigation />
       <div className={classes.container}>
+        <div className={classes.bannerImage}>
+          <img src="Banners/Desktop - 1.png" className={classes.bannerImage} />
+        </div>
         <div className={classes.intorParagraph}>
           <h1>The who's who in Logistics</h1>
           <p className={classes.introText}>
@@ -98,14 +119,16 @@ export default function Home() {
             </GridListTile>
             {services.map((service) => (
               <GridListTile key={service.serviceName}>
-                <img src={service.imgUrl} />
+                <Link key={service.serviceName} href={service.serviceUrl}>
+                  <img src={service.imgUrl} className={classes.serviceImage} />
+                </Link>
                 <GridListTileBar title={service.serviceName} />
               </GridListTile>
             ))}
           </GridList>
         </div>
         <div className={classes.paragraphTwo}>
-          <p>We are the best</p>
+          <ContactForm />
         </div>
       </div>
       <Footer />
